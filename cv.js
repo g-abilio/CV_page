4c23cfa975c336deb1995ed899e48bf3daf7b048
g-abilio <gabrielabiliobferreira2@gmail.com>
2024-05-27 00:47:09 -0300

    slider_table

--- cv.js
+++ cv.js
@@ -25,3 +25,22 @@ function welcome_en() {
 function welcome_pt_br() {
     alert("Seja bem vindo ao Curriculum Vitae de Gabriel!");
 }
+
+// function for the slider
+
+function showColumn(value) {
+    const columns = ["col_2022", "col_2023", "col_2024"];
+    const infos = ["info_2022", "info_2023", "info_2024"];
+
+    // hide all columns
+    for (var i = 0; i < columns.length; i++) {
+        document.getElementById(columns[i]).style.display = "none";
+        document.getElementById(infos[i]).style.display = "none";
+    }
+
+    // show the selected column and make it bold
+    document.getElementById(columns[value]).style.display = "table-cell";
+    document.getElementById(infos[value]).style.display = "table-cell";
+    document.getElementById(columns[value]).className = "th_new";
+    document.getElementById(infos[value]).className = "td_new";
+}
