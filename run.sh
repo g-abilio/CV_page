080d5e63ddc93c283359823bcc819410c30b0906
g-abilio <gabrielabiliobferreira2@gmail.com>
2024-07-13 21:26:59 -0300

    shell script now works for linux and windows as well

--- run.sh
+++ run.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
 # run html file
 
-open cv.html
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    browser="firefox" 
+
+elif [[ "$(uname -s)" == "Darwin" ]]; then
+    browser="open"
+
+elif [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+    browser="start"
+
+else
+    echo "Unsupported operating system."
+    exit 1
+fi
+
+$browser "cv.html"
